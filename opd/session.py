21268f9d05f44e4559852b0a67a2dd95860be791
Alexey Butyrev <butirev@gmail.com>
2022-09-18 00:26:44 -0400

    add creation table functionality

--- opd/session.py
+++ opd/session.py
@@ -1,12 +1,16 @@
 import json
 import datetime
 from collections import defaultdict
+from itertools import combinations
 
 # from detection import Detection
 from . import Detection
 from itertools import product
 from math import inf, sqrt
 from typing import Optional, Tuple
+
+import pandas as pd
+import numpy as np
 
 # Common types
 FrameID = int
@@ -15,6 +19,7 @@ DateTime = datetime.datetime
 Coordinate2D = Tuple[float, float]
 Speed2D = Tuple[float, float]
 DetectionID = str
+DataFrame = pd.DataFrame
 
 # Euclidian distance between 2 objects in 2D
 dist = lambda x, y: sqrt((x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]))
@@ -30,6 +35,22 @@ class Session:
         with open(filename) as f:
             data = json.load(f)
 
+        self.stat_table = pd.DataFrame(
+            {
+                "frame": [],
+                "timestamp": [],
+                "obj1_id": [],
+                "obj2_id": [],
+                "obj1_type": [],
+                "obj2_type": [],
+                "x1": [],
+                "y1": [],
+                "x2": [],
+                "y2": [],
+                "distance": [],
+                "speed": [],
+            }
+        )
         self.data = []
         self.detections = {}
 
@@ -48,7 +69,10 @@ class Session:
                     self.framesobjects[frame_id].add(object_id)
                     self.detectionsobjects[detection_class].add(object_id)
 
-                    x, y = o["floor_coordinates"]["X"], o["floor_coordinates"]["Y"]
+                    x, y = (
+                        o["warehouse_coordinates"]["X"],
+                        o["warehouse_coordinates"]["Y"],
+                    )
                     filtered_detections.append(o)
 
                     if object_id not in self.detections:
@@ -63,7 +87,54 @@ class Session:
                     else:
                         self.detections[object_id].add_frame(frame_id, ts, x, y)
             d["detections"] = filtered_detections
+            self.stat_table = self.__add_stat_row(
+                self.stat_table, self.detections, self.framesobjects[frame_id], frame_id
+            )
             self.data.append(d)
+
+    def __add_stat_row(
+        self, df: DataFrame, detections: dict, framesobjects: dict, frame_id: FrameID
+    ) -> DataFrame:
+        # less than one object on the frame
+        if len(framesobjects) < 2 or not detections:
+            return df
+
+        for id1, id2 in combinations(sorted(framesobjects), 2):
+            d1 = detections[id1]
+            d2 = detections[id2]
+            x1, y1 = d1.get_coord(frame_id)
+            x2, y2 = d2.get_coord(frame_id)
+            ts = d1.get_timestamp(frame_id)
+            distance = dist([x1, y1], [x2, y2])
+
+            search_slice = (df["obj1_id"] == id1) & (df["obj2_id"] == id2)
+            if any(search_slice):
+                distance_before = float(df[search_slice].iloc[[-1]]["distance"])
+                frame_before = FrameID(df[search_slice].iloc[[-1]]["frame"])
+                ts_before = d1.get_timestamp(frame_before)
+                dd = distance - distance_before
+                dt = (ts - ts_before).total_seconds()
+                speed = dd / dt
+            else:
+                speed = np.nan
+            df_to_add = pd.DataFrame(
+                {
+                    "frame": [frame_id],
+                    "timestamp": [ts],
+                    "obj1_id": [id1],
+                    "obj2_id": [id2],
+                    "obj1_type": [d1.detection_class],
+                    "obj2_type": [d2.detection_class],
+                    "x1": [x1],
+                    "y1": [y1],
+                    "x2": [x2],
+                    "y2": [y2],
+                    "distance": [distance],
+                    "speed": [speed],
+                }
+            )
+            df = pd.concat([df, df_to_add])
+        return df
 
     def detect_forklifts_and_people(
         self, frame_id: Optional[FrameID] = None
--- tests/test_session.py
+++ tests/test_session.py
@@ -7,13 +7,17 @@ def session():
     return Session(
         filename="metadata.json",
     )
+
+
 def test_type_check(session):
     assert isinstance(session, Session)
 
+
 def test_class_load_check(session):
     assert len(session.data) == 1579
-    assert str(session.min_time) == '2022-03-23T00:00:00.066667'
-    assert str(session.max_time) == '2022-03-23T00:01:45.266667'
+    assert str(session.min_time) == "2022-03-23T00:00:00.066667"
+    assert str(session.max_time) == "2022-03-23T00:01:45.266667"
+
 
 def test_detect_forklifts_and_people(session):
     forklifts, people = session.detect_forklifts_and_people(337)
@@ -21,10 +25,17 @@ def test_detect_forklifts_and_people(session):
     assert len(forklifts) == 2
     assert len(people) == 1
 
+
 def test_get_distances(session):
     d = session.get_distances(337)
 
     assert len(d) == 2
-    d = session.get_distances(337,2)
+    d = session.get_distances(337, 2)
     assert len(d) == 2
 
+
+def test_data_frame(session):
+    df = session.stat_table
+    assert len(set(df["obj2_type"])) == 2
+    assert len(set(df["obj1_type"])) == 2
+    assert df.shape[1] == 12
